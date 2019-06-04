7963228e22e1dafbe7c02eb96240dd6addbe0b53
Mateo ReyRosa <reyrosam@flip1.engr.oregonstate.edu>
2019-06-03 20:34:55 -0700

    added sign in for admin and users, sessions and 4 queries for db for password and username storing

--- server.js
+++ server.js
@@ -3,18 +3,33 @@ var express = require('express');
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 var bodyParser = require('body-parser');
-//var flash = require('express-flash');
-//var cookieParser = require('cookie-parser');
+var flash = require('express-flash');
+var cookieParser = require('cookie-parser');
 var session = require('express-session');
 //var validator = require('express-validator');
-
+var morgan = require('morgan');
   var path = require('path');
-app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(bodyParser.json());
-app.use(session({secret:'SuperSecretPassword'}));
-//  app.use(flash());
-
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use(session({
+    key: 'user_sid',
+    secret: 'somerandonstuffs',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        expires: 600000
+    }
+}));
+  var mysql = require('mysql');
+  var pool = mysql.createPool({
+    connectionLimit : 10,
+    host            : 'classmysql.engr.oregonstate.edu',
+    user            : 'cs340_reyrosam',
+    password        : '6249',
+    database        : 'cs340_reyrosam',
+    "dateStrings": true
+  });
 // prepare server
  // redirect API calls
  app.use('/', express.static(__dirname + '/www')); // redirect root
@@ -24,15 +39,216 @@ app.use(session({secret:'SuperSecretPassword'}));
 app.engine('handlebars', handlebars.engine);
 app.use(express.static('Pictures'));
 app.use(express.static('CSS'));
+app.use(cookieParser());
 app.set('view engine', 'handlebars');
 app.set('port', 3881);
+/*Table names*/
+var admin_table = "dsfsd";
+var user_table = "fds";
+var admin_request = "shrimp";
+var contact_table = "dasdsa";
+
+app.use(flash());
+app.use((req, res, next) => {
+    if (req.cookies.user_sid && !req.session.user) {
+        res.clearCookie('user_sid');
+    }
+    next();
+});
+app.use(session({
+    key: 'user_sid',
+    secret: 'somerandonstuffs',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        expires: 600000
+    }
+}));
+var sessionChecker = (req, res, next) => {
+    if (req.session.user && req.cookies.user_sid) {
+        res.redirect('/home');
+    } else {
+        next();
+    }
+};
+/*Sign up and Sign in methods*/
+app.post('/AdminSignUp', function(req, res){
+  var error =  {};
+  var context = {};
+  context.ContactName = req.body.ContactName;
+  context.ContactEmail = req.body.ContactEmail;
+  context.Username = req.body.Username;
+  context.Password = req.body.Password;
+  pool.query("INSERT INTO "+ admin_request + " (name, email, username, password) VALUES (?, ?, ?, ?)",
+   [req.body.ContactName, req.body.ContactEmail, req.body.Username, req.body.Password], function(err, result){
+    if(err){
+      error = err;
+      console.log(error);
+    }
+
+  });
+  if(error != {}){
+
+    req.flash('danger', 'Your sign up request failed. Please try again.')
+       res.render('adminsignup', context);
+  }else{
+
+    req.flash('success', 'Thank you ' + context.ContactName + '! You will be notified when your request is approved')
+        res.render('adminsignin', context);
+  }
+});
+
+app.get('/AdminSignUp', function(req, res){
+res.render('adminsignup');
+});
+
+app.post('/AdminSignIn', function(req, res){
+  var error =  {};
+  var context = {};
+
+  context.Username = req.body.Username;
+  context.Password = req.body.Password;
+  pool.query("SELECT * FROM "+ admin_table + " WHERE username = ? AND password = ?",
+   [req.body.Username, req.body.Password], function(err, result){
+error = err;
+console.log(error);
+if (result == undefined || result.length != 1){
+error = {error: "number of rows was not 1" }
+
+}else{
+
+req.session.username = result[0].username;
+}
+console.log(error);
+  });
+  if(error != {}){
+
+    req.flash('danger', 'No user with that password was found')
+       res.render('adminsignin', context);
+  }else{
+
+    req.flash('success', 'Logged In')
+        res.render('/home', context);
+  }
+});
+app.get('/AdminSignIn', function(req, res){
+res.render('adminsignin');
+});
 
 
 
+app.post('/UserSignUp', function(req, res){
+  var error =  {};
+  var context = {};
+  context.inputName = req.body.inputName;
+  context.inputEmail = req.body.inputEmail;
+  context.inputUsername = req.body.inputUsername;
+  context.inputPassword = req.body.inputPassword;
+  pool.query("INSERT INTO "+ user_table + " (name, email, username, password) VALUES (?, ?, ?, ?)",
+   [req.body.inputName, req.body.inputEmail, req.body.inputUsername, req.body.inputPassword], function(err, result){
+    if(err){
+      error = err;
+      console.log(error);
+    }
+
+  });
+  if(error != {}){
+
+    req.flash('danger', 'Your sign up failed. Please try again.')
+       res.render('usersignup', context);
+  }else{
+
+    req.flash('success', 'Thank you ' + context.inputName + '! Please Sign in. ')
+        res.render('usersignin', context);
+  }
+});
+app.get('/UserSignUp', function(req, res){
+res.render('usersignup');
+});
 
 
-app.get('/',function(req,res){
- res.render('home');
+
+app.post('/UserSignIn', function(req, res){
+  var error =  {};
+  var context = {};
+
+  context.Username = req.body.inputUsername;
+  context.Password = req.body.inputPassword;
+  pool.query("SELECT * FROM "+ user_table + " WHERE username = ? AND password = ?" ,
+   [req.body.inputUsername, req.body.inputPassword], function(err, result){
+error = err;
+console.log(error);
+if (result == undefined || result.length != 1){
+error = {error: "number of rows was not 1" }
+
+}else{
+
+req.session.username = result[0].username;
+}
+console.log(error);
+  });
+  if(error != {}){
+
+    req.flash('danger', 'No user with that password was found')
+       res.render('usersignin', context);
+  }else{
+
+    req.flash('success', 'Logged In')
+        res.render('/home', context);
+  }
+});
+app.get('/AdminSignIn', function(req, res){
+res.render('adminsignin');
+});
+app.get('/UserSignIn', function(req, res){
+res.render('usersignin');
+});
+
+app.get('/', function(req, res){
+res.redirect('/UserSignIn');
+
+});
+app.get('/home',function(req,res){
+  if (req.session.user && req.cookies.user_sid) {
+         res.render('home');
+    } else {
+    res.redirect('/UserSignIn');
+    }
+
+});
+app.get('/logout', (req, res) => {
+    if (req.session.user && req.cookies.user_sid) {
+        res.clearCookie('user_sid');
+        res.redirect('/');
+    } else {
+        res.redirect('/UserSignIn');
+    }
+});
+
+app.post('/ContactUs', function(req, res){
+  var context = {};
+  context.ContactName = req.body.ContactName;
+  context.ContactEmail = req.body.ContactEmail;
+  context.Message = req.body.Message;
+  pool.query("INSERT INTO "+ contact_table + " (name, email, message) VALUES (?, ?, ?)",
+   [req.body.ContactName, req.body.ContactEmail, req.body.Message], function(err, result){
+    if(err){
+      error = err;
+      console.log(error);
+    }
+
+  });
+  if(error != {}){
+
+    req.flash('success', 'An error occured, please try again.')
+       res.render('contactus', context);
+  }else{
+
+    req.flash('success', 'Thank you ' + context.ContactName + '! We will be in touch.')
+        res.render('contactus', context);
+  }
+
+
 });
 
 app.get('/ContactUs',function(req,res){
